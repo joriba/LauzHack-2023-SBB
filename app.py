ad510583ca8fb619a7aeb3055554dc6495e1b87a
Oskar Dabkowski <oskar.dabkowski@polytechnique.edu>
2023-12-03 00:24:38 +0100

    Extra field + <br>

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from markupsafe import Markup
 from canton import Canton
 from gmaps_helper import geocode
 from parking_spots_finder import closest_by_car_time
@@ -9,10 +10,14 @@ def index():
     if request.method == 'POST':
         user_input1 = request.form['user_input1']
         user_input2 = request.form['user_input2']
+        user_input3 = request.form['user_input3']
         # Process the user input as needed
         coords = geocode(user_input1)[0]['geometry']['location']
-        arr = [sub_array[2] for sub_array in closest_by_car_time(coords['lat'], coords['lng'], 23)]
-        processed_result = f"Locations of {user_input1} and {user_input2}: {arr}"
+        arr = [(sub_array[0] + ' ' + sub_array[2]['text']) for sub_array in closest_by_car_time(coords['lat'], coords['lng'], 7)]
+
+        formatted_arr = Markup('<br>'.join(map(str, arr)))
+
+        processed_result = f"Closest train stations to {user_input1} to travel by car: <br> {formatted_arr}"
         return render_template('sbb_result.html', result=processed_result)
     return render_template('sbb_index.html')
 
