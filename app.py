67ee7ba2c210f9faac7029aae782940ca5378317
Oskar Dabkowski <oskar.dabkowski@polytechnique.edu>
2023-12-02 21:22:48 +0100

    Double input

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
 from canton import Canton
+import gmaps_helper
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        user_input = request.form['user_input']
+        user_input1 = request.form['user_input1']
+        user_input2 = request.form['user_input2']
         # Process the user input as needed
-        processed_result = f'Canton of {user_input}: {Canton(user_input)}'
-        return render_template('result.html', result=processed_result)
-    return render_template('index.html')
+        processed_result = f'Canton of {user_input1} and {user_input2}: {Canton(user_input1), Canton(user_input2),}'
+        return render_template('sbb_result.html', result=processed_result)
+    return render_template('sbb_index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
