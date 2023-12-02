241280c199b4e6e9bf17d2a6cc02e6c3424afaeb
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2023-12-02 21:14:02 +0100

    add api to geolocate

--- gmaps_helper.py
+++ gmaps_helper.py
@@ -5,10 +5,7 @@ import os
 load_dotenv()
 API_KEY = os.getenv("GMAPS_API_KEY")
 BASE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json"
-#   ?destinations=<addresses, separated by |>
-#   &origins=<starting point>
-#   &units=metric
-#   &key=AIzaSyC4BLR0fxjI9NaKTUBG6V2PcZYEK32GPlo
+gmaps = googlemaps.Client(key=API_KEY)
 
 BASE_PARAMS = {
     "units": "metric",
@@ -16,5 +13,7 @@ BASE_PARAMS = {
 }
 
 def get_closest_by_car(origin, destinatinons: list[str]):
-    gmaps = googlemaps.Client(key=API_KEY)
     return gmaps.distance_matrix([origin], destinatinons)
+
+def geolocate(origin: str):
+    return gmaps.geolocate(origin)
