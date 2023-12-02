4fa6a21cf5f9ab53603c925dad7823cf7d4181f8
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2023-12-02 18:57:47 +0100

    add api for google maps

--- demo.py
+++ demo.py
@@ -1,5 +1,6 @@
 from journey_service_helper import get_place_by_name, get_trip_between_place_ids
 from journey_maps_helper import get_journey_for_trip_id
+from gmaps_distance_helper import get_closest_by_car
 
 geneva_id = get_place_by_name("Genève").id
 zurich_id = get_place_by_name("Zürich").id
@@ -11,3 +12,6 @@ print([x.duration for x in trip.legs])
 
 r = get_journey_for_trip_id(trip.id)
 print(len(r.features))
+
+distances = get_closest_by_car("1020 Renens", ["Lausanne"])
+print(distances)
---
+++ gmaps_distance_helper.py
@@ -0,0 +1,20 @@
+import googlemaps
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+API_KEY = os.getenv("GMAPS_API_KEY")
+BASE_URL = "https://maps.googleapis.com/maps/api/distancematrix/json"
+#   ?destinations=<addresses, separated by |>
+#   &origins=<starting point>
+#   &units=metric
+#   &key=AIzaSyC4BLR0fxjI9NaKTUBG6V2PcZYEK32GPlo
+
+BASE_PARAMS = {
+    "units": "metric",
+    "key": API_KEY,
+}
+
+def get_closest_by_car(origin: str, destinatinons: list[str]):
+    gmaps = googlemaps.Client(key=API_KEY)
+    return gmaps.distance_matrix([origin], destinatinons)
