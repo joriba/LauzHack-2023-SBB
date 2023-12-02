36e7e1357e5ea25d6265bc2112f0e4ff29006cf1
Natali124 <nataligogishvili@gmail.com>
2023-12-02 16:01:10 +0100

    example input website

--- Website/app.py
+++ Website/app.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return 'Hello, World!'
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        user_input = request.form['user_input']
+        # Process the user input as needed
+        processed_result = f'You entered: {user_input}'
+        return render_template('result.html', result=processed_result)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
