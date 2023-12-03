b6cc82cde0f9939e34eb5f99f4ebdf2fd3bf356b
Natali124 <nataligogishvili@gmail.com>
2023-12-03 02:08:39 +0100

    app

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template, request
 from markupsafe import Markup
 from canton import Canton
-from gmaps_helper import geocode
-from parking_spots_finder import closest_by_car_time
+from gmaps_helper import geocode, coordinate
+from parking_spots_finder import closest_by_car_time, all_trips_for_departure_time
+import datetime
+import time
+from datetime import datetime
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
@@ -11,18 +15,30 @@ def index():
         user_input1 = request.form['user_input1']
         user_input2 = request.form['user_input2']
         user_input3 = request.form['user_input3']
+        user_input4 = request.form['user_input4']
+        
+        date_format = "%Y-%m-%dT%H:%M"
+        if user_input3 != "":
+            user_input3 = datetime.strptime(user_input3, date_format)
+        else:
+            user_input3 = datetime.now()
+        if user_input4 != "":
+            user_input4 = datetime.strptime(user_input4, date_format)
+
+        # user_input4 = request.form['user_input4']
         # Process the user input as needed
-        coords = geocode(user_input1)[0]['geometry']['location']
-        arr = [(sub_array[0] + ' ' + sub_array[2]['text']) for sub_array in closest_by_car_time(coords['lat'], coords['lng'], 7)]
+        origin_lat,  origin_lon = coordinate(user_input1)
+        dest_lat, dest_lon = coordinate(user_input2)
 
-        formatted_arr = Markup('<br>'.join(map(str, arr)))
+        start_time = user_input3
+        
+        arr = all_trips_for_departure_time(start_time, origin_lat, origin_lon, dest_lat, dest_lon, 5)[0]
 
-        processed_result = f"Closest train stations to {user_input1} to travel by car: <br> {formatted_arr}"
+        # formatted_arr = Markup('<br>'.join(map(str, arr)))
+
+        processed_result = f"Closest train stations to {user_input1} to travel by car: <br> {arr}"
         return render_template('sbb_result.html', result=processed_result)
     return render_template('sbb_index.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
-
-
-   
