4c367d855e5bfac44675dd6caf9815b28444e4f3
Jonas Rindlisbacher <jonasrindlisbacher@pm.me>
2023-12-03 01:14:30 +0100

    add "distance by car" column to output of all trips

--- parking_spots_finder.py
+++ parking_spots_finder.py
@@ -11,14 +11,14 @@ def car_times_from_parking_spots(origin, parking_spots: list[dict]) -> list[dict
     for i in range(len(destinations)):
         name = points["destination_addresses"][i]
         coords = destinations[i]
-        distance = points["rows"][0]["elements"][i]["duration"]
+        distance = points["rows"][0]["elements"][i]
         distances.append((name, coords, distance))
 
     return distances
 
 def closest_by_car_time(lat, lon, count):
     result = car_times_from_parking_spots((lat, lon), parkplace_helper.closest_parkings(lat, lon, count))
-    result.sort(key=lambda x: x[2]["value"])
+    result.sort(key=lambda x: x[2]["duration"]["value"])
     return result
 
 def all_trips_for_arrival_time(arrival_time, origin_lat, origin_lon, dest_lat, dest_lon, nb_parkings):
@@ -32,11 +32,12 @@ def all_trips_for_arrival_time(arrival_time, origin_lat, origin_lon, dest_lat, d
         for trip in trips:
             departure_time = journey_service_helper.get_trip_departure_time(trip)
             car_arrival_time = departure_time - TRANSFER_TIME
-            car_departure_time = car_arrival_time - datetime.timedelta(seconds=spot[2]["value"])
+            car_departure_time = car_arrival_time - datetime.timedelta(seconds=spot[2]["duration"]["value"])
             result.append({
                 "Departure Time": car_departure_time,
                 "Arrival Time": journey_service_helper.get_trip_arrival_time(trip),
-                "Parking Spot": spot[0]
+                "Parking Spot": spot[0],
+                "Distance by Car": spot[2]
             }) 
         
     return result
@@ -48,23 +49,27 @@ def all_trips_for_departure_time(start_time, origin_lat, origin_lon, dest_lat, d
         coords = spot[1]
         origin_string = f'{[coords[1], coords[0]]}'.replace(" ", "")
         dest_string = f'{[dest_lon, dest_lat]}'.replace(" ", "")
-        train_departure_time = start_time + spot[2]["value"] + TRANSFER_TIME
+        train_departure_time = start_time + spot[2]["duration"]["value"] + TRANSFER_TIME
         trips = journey_service_helper.get_trip_between_place_ids(origin_string, dest_string, train_departure_time, False)
         for trip in trips:
             arrival_time = journey_service_helper.get_trip_arrival_time(trip)
             train_departure_time = journey_service_helper.get_trip_departure_time(trip)
             car_arrival_time = train_departure_time - TRANSFER_TIME
-            car_departure_time = car_arrival_time - datetime.timedelta(seconds=spot[2]["value"])
+            car_departure_time = car_arrival_time - datetime.timedelta(seconds=spot[2]["duration"]["value"])
             result.append({
                 "Departure Time": car_departure_time,
                 "Arrival Time": arrival_time,
-                "Parking Spot": spot[0]
+                "Parking Spot": spot[0],
+                "Distance by Car": spot[2]
             }) 
         
     return result
 
 this_afternoon = datetime.datetime.fromtimestamp(time.time()) - datetime.timedelta(hours=8)
-print(all_trips_for_arrival_time(this_afternoon, 46.5203, 6.566, 46.9467632, 7.4409836, 10))
+coords_lausanne = gmaps_helper.coordinate("Ecublens")
+coords_bolligen = gmaps_helper.coordinate("Bolligen")
+for trip in all_trips_for_arrival_time(this_afternoon, coords_lausanne[0], coords_lausanne[1], coords_bolligen[0], coords_bolligen[1], 10):
+    print(trip, "\n")
 # print(closest_parkings(46.5203, 6.566, 10))
 # 46.9467632, 7.4409836
 # coords = gmaps_helper.geocode("Chem. des Fleurs de Lys, 1350 Orbe")[0]['geometry']['location']
