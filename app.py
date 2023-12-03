3be6dbe2ac54aba1bd9c31c354115944ab7ab089
Natali124 <nataligogishvili@gmail.com>
2023-12-03 01:36:51 +0100

    fixed html

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def index():
     if request.method == 'POST':
         user_input1 = request.form['user_input1']
         user_input2 = request.form['user_input2']
+        user_input3 = request.form['user_input3']
         # Process the user input as needed
         processed_result = f'Canton of {user_input1} and {user_input2}: {Canton(user_input1), Canton(user_input2),}'
         return render_template('sbb_result.html', result=processed_result)
